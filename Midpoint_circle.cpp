f289af0b50a636bac5830cb26d9c8d9ba673da92
JonnyMia <JohnnyMia1@proton.me>
2025-05-05 23:22:11 +0530

    Midpoint_circle.cpp

--- Midpoint_circle.cpp
+++ Midpoint_circle.cpp
@@ -1,53 +1,67 @@
-#include <stdio.h>
-#include <GL/gl.h>
 #include <GL/glut.h>
+#include <iostream>
+using namespace std;
+
 int xc, yc, r;
+
 void plotCirclePoints(int x, int y) {
-glBegin(GL_POINTS);
-glVertex2i(xc + x, yc + y);
-glVertex2i(xc - x, yc + y);
-glVertex2i(xc + x, yc - y);
-glVertex2i(xc - x, yc - y);glVertex2i(xc + y, yc + x);
-glVertex2i(xc - y, yc + x);
-glVertex2i(xc + y, yc - x);
-glVertex2i(xc - y, yc - x);
-glEnd();
+    glVertex2i(xc + x, yc + y);
+    glVertex2i(xc - x, yc + y);
+    glVertex2i(xc + x, yc - y);
+    glVertex2i(xc - x, yc - y);
+    glVertex2i(xc + y, yc + x);
+    glVertex2i(xc - y, yc + x);
+    glVertex2i(xc + y, yc - x);
+    glVertex2i(xc - y, yc - x);
 }
-void MidpointCircle() {
-int x = 0, y = r;
-int p = 1 - r; // Initial decision parameter
-plotCirclePoints(x, y);
-while (x < y) {
-if (p < 0) {
-x++;
-p += 2 * x + 1;
-} else {
-x++;
-y--;
-p += 2 * (x - y) + 1;
+
+void drawCircle() {
+    int x = 0, y = r;
+    int p = 1 - r;
+
+    glClear(GL_COLOR_BUFFER_BIT); // Clear screen
+    glBegin(GL_POINTS); // Start drawing
+
+    plotCirclePoints(x, y);
+
+    while (x < y) {
+        if (p < 0) {
+            x++;
+            p += 2 * x + 1;
+        } else {
+            x++;
+            y--;
+            p += 2 * (x - y) + 1;
+        }
+        plotCirclePoints(x, y);
+    }
+    
+    glEnd(); // Finish drawing
+    glFlush(); // Force render
 }
-plotCirclePoints(x, y);
+
+void init() {
+    glClearColor(1, 1, 1, 1); // White background
+    glColor3f(0, 0, 0);       // Black points
+    gluOrtho2D(0.0, 640.0, 0.0, 480.0); // Set coordinate system
 }
-}
-void display() {
-glClear(GL_COLOR_BUFFER_BIT);
-MidpointCircle();
-glFlush();
-glutSwapBuffers();
-}
-void myinit() {
-glClearColor(0.0, 0.0, 0.0, 0.0);
-glMatrixMode(GL_PROJECTION);
-glLoadIdentity();
-gluOrtho2D(0.0, 640.0, 0.0, 480.0);
-}
+
 int main(int argc, char **argv) {
-xc = 200, yc = 200, r = 80;
-glutInit(&argc, argv);
-glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-glutInitWindowSize(640, 480);
-glutCreateWindow("Midpoint Circle Drawing Algorithm");
-myinit();
-glutDisplayFunc(display);
-glutMainLoop();
+    cout << "Enter center (xc yc): ";
+    cin >> xc >> yc;
+    cout << "Enter radius: ";
+    cin >> r;
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); // Single buffer
+    glutInitWindowSize(640, 480);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Midpoint Circle Drawing Algorithm");
+
+    init();
+    glutDisplayFunc(drawCircle);
+    glutMainLoop();
+
+    return 0;
 }
+
