77f35a087164ecf86f92ac34a2f2f063f9090d17
JonnyMia <JohnnyMia1@proton.me>
2025-05-05 23:21:11 +0530

    BHRE_LINE.cpp

--- BHRE_LINE.cpp
+++ BHRE_LINE.cpp
@@ -1,12 +1,11 @@
-#include <stdio.h>
-#include <iostream>
-#include <GL/gl.h>
 #include <GL/glut.h>
+#include <iostream>
 
 using namespace std;
-int x_start, y_start, x_end, y_end; // Renamed variables to avoid conflicts
 
-void display() {
+int x_start, y_start, x_end, y_end;
+
+void drawLine() {
     float dx, dy, x, y, p;
     dx = x_end - x_start;
     dy = y_end - y_start;
@@ -14,12 +13,11 @@ void display() {
     x = x_start;
     y = y_start;
 
-    glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_POINTS);
-    glVertex2i(x, y); // Plot the initial point
-    glEnd();
+    glClear(GL_COLOR_BUFFER_BIT); // Clear screen
+    glBegin(GL_POINTS); // Begin drawing points
 
-    // Bresenham's Line Algorithm
+    glVertex2i(x, y); // Plot the initial point
+
     while (x < x_end) {
         if (p < 0) {
             p = p + (2 * dy);  // Move horizontally
@@ -29,44 +27,34 @@ void display() {
             x++;
             y++;
         }
-
-        // Plot the point at (x, y)
-        glBegin(GL_POINTS);
-        glVertex2i(x, y);
-        glEnd();
+        glVertex2i(x, y); // Plot the next point
     }
 
-    glFlush();
-    glutSwapBuffers();
+    glEnd(); // End drawing
+    glFlush(); // Force render
 }
 
-void myinit() {
-    glClearColor(0.0, 0.0, 0.0, 0.0); // Set background color to black
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluOrtho2D(0.0, 640.0, 0.0, 480.0); // Set the coordinate system
+void init() {
+    glClearColor(1, 1, 1, 1); // White background
+    glColor3f(0, 0, 0);       // Black line
+    gluOrtho2D(0.0, 640.0, 0.0, 480.0); // Set coordinate system
 }
 
 int main(int argc, char** argv) {
-  
-    cout << "Enter x1";
-    cin>>x_start;
-    cout << "Enter x2";
-    cin>>x_end;
-    
-    cout << "Enter y1";
-    cin>>y_start;
-    cout << "Enter y2";
-    cin>>y_end;
+    cout << "Enter x1 y1: ";
+    cin >> x_start >> y_start;
+    cout << "Enter x2 y2: ";
+    cin >> x_end >> y_end;
 
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB); // Use double buffering and RGB color mode
-    glutInitWindowSize(640, 480); // Set window size
-    glutCreateWindow("Bresenham's Line Algorithm"); // Create the window
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); // Single buffer
+    glutInitWindowSize(640, 480);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Bresenham's Line");
 
-    myinit();
-    glutDisplayFunc(display); // Register the display function
-    glutMainLoop(); // Start the main loop
+    init();
+    glutDisplayFunc(drawLine);
+    glutMainLoop();
 
     return 0;
 }
